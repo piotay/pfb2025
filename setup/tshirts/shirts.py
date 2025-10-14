744092ec43a44e73d45aa96e1374c02b0aa218ff
Jessen V. Bredeson <jessenbredeson@berkeley.edu>
2025-10-14 13:59:33 -0400

    Updating shirts.py and shirts.pl scripts

--- setup/tshirts/shirts.py
+++ setup/tshirts/shirts.py
@@ -11,30 +11,74 @@ mens	large	Graphite Heather
 mens	large	sport grey
 """
 
+import os
 import sys
 
-shirts = {}
-with open("shirts.txt","r") as file_object: 
-  for line in file_object:
-    line = line.rstrip()
-    [style, size, color] = line.split("\t")
-    style = style.strip().lower()
-    size = size.strip().lower()
-    color = color.strip().lower()
-    if style not in shirts:
-        shirts[style] = {}
-    if size not in shirts[style]:
-        shirts[style][size] = {}
-    if color not in shirts[style][size]:
-        shirts[style][size][color] = 0
 
-    shirts[style][size][color] += 1
+def usage(message=None, exitcode=1, stream=sys.stderr):
+  message = '' if message is None else f'\n\nERROR: {message}\n\n'
 
-for style in shirts:
-  for size in shirts[style]:
-    for color in shirts[style][size]:
-      count = shirts[style][size][color]
-      print(style,size,color,count,sep="\t")
+  stream.write("\n")
+  stream.write("Usage: %s <input.tsv>\n\n" % os.path.basename(sys.argv[0]))
+  stream.write(
+    "Notes:\n"
+    " - input.tsv is a TSV file containing three columns (additional\n"
+    "   columns are ignored) in the format: <mens|womens>\\t<size>\\t<color>\n"
+  )
+  stream.write(message)
+  
+  sys.exit(exitcode)
 
 
-print(shirts, file=sys.stderr)
+  
+def main(argv):
+  if len(argv) != 1:
+    usage('Unexpected number of arguments')
+  
+  shirts = {}
+  with open(argv[0], "r") as file_object: 
+    for line in file_object:
+      if line.lstrip().startswith("#"):
+        continue
+      
+      fields = line.strip().split("\t")
+      
+      style = fields[0].strip().lower().replace("'","")  # men's | women's
+      size = fields[1].strip().lower()
+      color = fields[2].strip().lower()
+
+      # normalize sizes of form large, xlarge, xl, xxl, 2xl, etc.
+      for s in ('small','medium','large'):
+        if size.endswith(s):
+          size = size[:size.index(s)] + s[0]
+
+      if 'x' in size:
+        if size[0] in set(map(str, range(10))):
+          pass
+        else:
+          xcount = size.count('x')
+          size = 'x' + size[-1]
+          if xcount > 1:
+            size = str(xcount) + size
+          
+      size = size.upper()
+        
+      if style not in shirts:
+          shirts[style] = {}
+      if size not in shirts[style]:
+          shirts[style][size] = {}
+      if color not in shirts[style][size]:
+          shirts[style][size][color] = 0
+      shirts[style][size][color] += 1
+
+  for style in shirts:
+    for size in shirts[style]:
+      for color in shirts[style][size]:
+        count = shirts[style][size][color]
+        print(style,size,color,count,sep="\t")
+      
+  # print(shirts, file=sys.stderr)
+
+
+    
+main(sys.argv[1:])
